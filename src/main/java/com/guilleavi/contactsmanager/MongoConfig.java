50f211a433c6fcd9bb80773e68e09ee85d8eece9
guilleavi <guillerminaavigliano@yahoo.com.ar>
2019-11-28 15:05:13 -0300

    use AbstractMongoClientConfiguration in MongoConfig

--- src/main/java/com/guilleavi/contactsmanager/MongoConfig.java
+++ src/main/java/com/guilleavi/contactsmanager/MongoConfig.java
@@ -1,20 +1,27 @@
 package com.guilleavi.contactsmanager;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.WriteResultChecking;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.guilleavi.contactsmanager.repositories")
-public class MongoConfig extends AbstractMongoConfiguration  {
+public class MongoConfig extends AbstractMongoClientConfiguration  {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private String port;
 
     @Override
     public MongoClient mongoClient() {
-        return  new MongoClient("localhost", 27017);
+        return MongoClients.create(host + ":" + port);
     }
 
     @Override
